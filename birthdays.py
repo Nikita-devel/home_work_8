da7c4a2b568ae613470e88d751da420020b43d58
Chumak Nikita <brseve90@gmail.com>
2023-06-13 18:01:30 +0200

    Update birthdays.py

--- birthdays.py
+++ birthdays.py
@@ -14,23 +14,25 @@ def get_birthdays_per_week(users):
 
         users_to_greet = []
         for user in users:
-            if user['birthday'].date() == current_day:
-                users_to_greet.append(user['name'])
+            if (user['birthday'].date().day, user['birthday'].date().month) == (current_day.day, current_day.month):
+                if user['birthday'].date().year != today.year:
+                    users_to_greet.append(user['name'])
             elif user['birthday'].date().weekday() > 4 and current_day.weekday() == 0:
                 if (user['birthday'].date() - timedelta(days=7)).day == current_day.day:
-                    users_to_greet.append(user['name'])
+                    if user['birthday'].date().year != today.year:
+                        users_to_greet.append(user['name'])
         if users_to_greet:
             print(f'{weekdays[day]}: {", ".join(users_to_greet)}')
 
 def main():
     users = [
-        {'name': 'Bill', 'birthday': datetime(2023, 6, 12)},
-        {'name': 'Jill', 'birthday': datetime(2023, 6, 12)},
-        {'name': 'Kim', 'birthday': datetime(2023, 6, 16)},
-        {'name': 'Jan', 'birthday': datetime(2023, 6, 16)},
-        {'name': 'John', 'birthday': datetime(2023, 6, 19)},
-        {'name': 'Jane', 'birthday': datetime(2023, 6, 21)},
-        {'name': 'Joe', 'birthday': datetime(2023, 6, 23)},
+        {'name': 'Bill', 'birthday': datetime(1990, 6, 12)},
+        {'name': 'Jill', 'birthday': datetime(1991, 6, 12)},
+        {'name': 'Kim', 'birthday': datetime(1992, 6, 16)},
+        {'name': 'Jan', 'birthday': datetime(1993, 6, 16)},
+        {'name': 'John', 'birthday': datetime(1994, 6, 19)},
+        {'name': 'Jane', 'birthday': datetime(1995, 6, 21)},
+        {'name': 'Joe', 'birthday': datetime(1996, 6, 23)},
     ]
     
     get_birthdays_per_week(users)
